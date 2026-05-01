e04117be8397d7c71fcd9b4a23e45f2b3296d847
opuadm <opuadm@gmail.com>
2026-05-01 20:07:56 +0200

    a small change to barely any syscalls

--- arch/x86_64/syscall_arch.h
+++ arch/x86_64/syscall_arch.h
@@ -11,36 +11,60 @@ static inline long __syscall0(long n)
 static inline long __syscall1(long n, long a1)
 {
 	unsigned long __ret;
-	__asm__ __volatile__ ("int $0x80" : "=a"(__ret) : "a"(n), "b"(a1) : "memory");
+	__asm__ __volatile__ (
+		"pushq %%rbx\n\t"
+		"movq %2, %%rbx\n\t"
+		"int $0x80\n\t"
+		"popq %%rbx"
+		: "=a"(__ret) : "a"(n), "r"(a1) : "memory");
 	return __ret;
 }
 
 static inline long __syscall2(long n, long a1, long a2)
 {
 	unsigned long __ret;
-	__asm__ __volatile__ ("int $0x80" : "=a"(__ret) : "a"(n), "b"(a1), "c"(a2) : "memory");
+	__asm__ __volatile__ (
+		"pushq %%rbx\n\t"
+		"movq %2, %%rbx\n\t"
+		"int $0x80\n\t"
+		"popq %%rbx"
+		: "=a"(__ret) : "a"(n), "r"(a1), "c"(a2) : "memory");
 	return __ret;
 }
 
 static inline long __syscall3(long n, long a1, long a2, long a3)
 {
 	unsigned long __ret;
-	__asm__ __volatile__ ("int $0x80" : "=a"(__ret) : "a"(n), "b"(a1), "c"(a2), "d"(a3) : "memory");
+	__asm__ __volatile__ (
+		"pushq %%rbx\n\t"
+		"movq %2, %%rbx\n\t"
+		"int $0x80\n\t"
+		"popq %%rbx"
+		: "=a"(__ret) : "a"(n), "r"(a1), "c"(a2), "d"(a3) : "memory");
 	return __ret;
 }
 
 static inline long __syscall4(long n, long a1, long a2, long a3, long a4)
 {
 	unsigned long __ret;
-	__asm__ __volatile__ ("int $0x80" : "=a"(__ret) : "a"(n), "b"(a1), "c"(a2), "d"(a3), "S"(a4) : "memory");
+	__asm__ __volatile__ (
+		"pushq %%rbx\n\t"
+		"movq %2, %%rbx\n\t"
+		"int $0x80\n\t"
+		"popq %%rbx"
+		: "=a"(__ret) : "a"(n), "r"(a1), "c"(a2), "d"(a3), "S"(a4) : "memory");
 	return __ret;
 }
 
 static inline long __syscall5(long n, long a1, long a2, long a3, long a4, long a5)
 {
 	unsigned long __ret;
-	__asm__ __volatile__ ("int $0x80"
-		: "=a"(__ret) : "a"(n), "b"(a1), "c"(a2), "d"(a3), "S"(a4), "D"(a5) : "memory");
+	__asm__ __volatile__ (
+		"pushq %%rbx\n\t"
+		"movq %2, %%rbx\n\t"
+		"int $0x80\n\t"
+		"popq %%rbx"
+		: "=a"(__ret) : "a"(n), "r"(a1), "c"(a2), "d"(a3), "S"(a4), "D"(a5) : "memory");
 	return __ret;
 }
 
@@ -48,12 +72,15 @@ static inline long __syscall6(long n, long a1, long a2, long a3, long a4, long a
 {
 	unsigned long __ret;
 	__asm__ __volatile__ (
-		"movq %%rbp, %%r10\n\t"
+		"pushq %%rbx\n\t"
+		"pushq %%rbp\n\t"
+		"movq %2, %%rbx\n\t"
 		"movq %7, %%rbp\n\t"
 		"int $0x80\n\t"
-		"movq %%r10, %%rbp"
+		"popq %%rbp\n\t"
+		"popq %%rbx"
 		: "=a"(__ret)
-		: "a"(n), "b"(a1), "c"(a2), "d"(a3), "S"(a4), "D"(a5), "g"(a6)
-		: "memory", "r10");
+		: "a"(n), "r"(a1), "c"(a2), "d"(a3), "S"(a4), "D"(a5), "r"(a6)
+		: "memory");
 	return __ret;
 }
