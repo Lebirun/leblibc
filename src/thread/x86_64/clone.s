1d29037dfee0d32ee8d5dcb871c25a225b57e889
opuadm <opuadm@gmail.com>
2026-08-03 23:08:07 +0200

    Update syscall number mappings and fix x86_64 clone argument and stack handling

--- src/thread/x86_64/clone.s
+++ src/thread/x86_64/clone.s
@@ -2,21 +2,28 @@
 .global __clone
 .type __clone,@function
 __clone:
-	xor %eax,%eax
+	and $-16,%rsi
+	sub $16,%rsi
+	mov %rdi,0(%rsi)
+	mov %rcx,8(%rsi)
+	mov 8(%rsp),%r10
+	push %rbx
 	mov $192 | 0x80000000,%eax
-	mov %rdi,%r11
-	mov %rdx,%rdi
-	mov %rsi,%rsi
+	mov %rdx,%rbx
+	mov %rsi,%rcx
 	mov %r8,%rdx
-	mov %r9,%r8
-	mov 8(%rsp),%r9
+	mov %r9,%rsi
+	mov %r10,%rdi
 	int $0x80
 	test %eax,%eax
 	jnz 1f
 	xor %ebp,%ebp
+	pop %r11
+	pop %rdi
 	call *%r11
 	mov %eax,%edi
 	xor %eax,%eax
 	int $0x80
 	hlt
-1:	ret
+1:	pop %rbx
+	ret
